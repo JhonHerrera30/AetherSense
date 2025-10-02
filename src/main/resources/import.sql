fba41eb6079bfd2fcef62fcbb6d0036b692c533e
LucaBussi <167002759+LucaBussi@users.noreply.github.com>
2025-10-02 21:54:13 +0200

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,44 +11,44 @@ insert into app_user(id, name, surname, date_of_birth, phone_number)values(3, 'L
 insert into app_user(id, name, surname, date_of_birth, phone_number)values(4, 'Jhon', 'Herrera', '2004-01-30', '3887258823');
 
 
--- 3. Credenziali (devono esistere gli utenti e il progetto)
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(1, 1, 1, 'fbalduini@icloud.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'SUPERADMIN', 'mimi16|SUPERADMIN', 'mimi16');
+-- 3. CDeviceenziali (devono esistere gli utenti e il progetto)
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(1, 1, 1, 'fbalduini@icloud.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'SUPERADMIN', 'mimi16|SUPERADMIN', 'mimi16');
 
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(2, 1, 2, 'flamy003@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_ADMIN', 'mimi|LTRAD', 'mimi');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(3, 51, 3, 'luca.bussi@outlook.it','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_ADMIN', 'kuca|FIRE', 'kuca');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(4, 101, 4, 'jhon30.herrera@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_ADMIN', 'jem|VOLCANO', 'jem');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(2, 1, 2, 'flamy003@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_ADMIN', 'mimi|LTRAD', 'mimi');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(3, 51, 3, 'luca.bussi@outlook.it','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_ADMIN', 'kuca|FIRE', 'kuca');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(4, 101, 4, 'jhon30.herrera@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_ADMIN', 'jem|VOLCANO', 'jem');
 
--- 3b. Associazione admin-credentials
-insert into admin(id, credentials_id) values (1, 2);   -- LTRAD_ADMIN
-insert into admin(id, credentials_id) values (2, 3);   -- FIRE_ADMIN
-insert into admin(id, credentials_id) values (3, 4);   -- VOLCANO_ADMIN
+-- 3b. Associazione admin-cDeviceentials
+insert into admin(id, cDeviceentials_id) values (1, 2);   -- LTRAD_ADMIN
+insert into admin(id, cDeviceentials_id) values (2, 3);   -- FIRE_ADMIN
+insert into admin(id, cDeviceentials_id) values (3, 4);   -- VOLCANO_ADMIN
 
-insert into superadmin(id, credentials_id) values (1, 1);
+insert into superadmin(id, cDeviceentials_id) values (1, 1);
 insert into superadmin_admin_emails(superadmin_id, admin_email) values (1, 'flamy003@gmail.com');
 insert into superadmin_admin_emails(superadmin_id, admin_email) values (1, 'luca.bussi@outlook.it');
 insert into superadmin_admin_emails(superadmin_id, admin_email) values (1, 'jhon30.herrera@gmail.com');
 
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(5, 1, null, 'flaminia.balduini@fastwebnet.it','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_OPERATOR', 'Flaminia|LTRAD', 'Flaminia');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(6, 51, null, 'lucabussi03@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_OPERATOR', 'Luca|FIRE', 'Luca');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(7, 101, null, 'jhonherrera30@icloud.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_OPERATOR', 'Jhon|VOLCANO', 'Jhon');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(8, 1, null, 'giulia.rossi@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_OPERATOR', 'Giulia|LTRAD', 'Giulia');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(9, 51, null, 'marco.verdi@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_OPERATOR', 'Marco|FIRE', 'Marco');
-insert into credentials(id, project_id, user_id, email, password, role, username, visible_username)values(10, 101, null, 'carlos.gomez@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_OPERATOR', 'Carlos|VOLCANO', 'Carlos');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(5, 1, null, 'flaminia.balduini@fastwebnet.it','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_OPERATOR', 'Flaminia|LTRAD', 'Flaminia');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(6, 51, null, 'lucabussi03@gmail.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_OPERATOR', 'Luca|FIRE', 'Luca');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(7, 101, null, 'jhonherrera30@icloud.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_OPERATOR', 'Jhon|VOLCANO', 'Jhon');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(8, 1, null, 'giulia.rossi@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'LTRAD_OPERATOR', 'Giulia|LTRAD', 'Giulia');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(9, 51, null, 'marco.verdi@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'FIRE_OPERATOR', 'Marco|FIRE', 'Marco');
+insert into cDeviceentials(id, project_id, user_id, email, password, role, username, visible_username)values(10, 101, null, 'carlos.gomez@example.com','$2a$10$cHSpwVdP8S33zR6PHIfvs.g9TZl6QbhHX9KTayP/91cxVScdb/c.W', 'VOLCANO_OPERATOR', 'Carlos|VOLCANO', 'Carlos');
 
 -- 3c. Associazione operatori-admin (employer)
-update credentials set employer_id = 1 where email = 'flaminia.balduini@fastwebnet.it';
-update credentials set employer_id = 1 where email = 'giulia.rossi@example.com';
-update credentials set employer_id = 2 where email = 'lucabussi03@gmail.com';
-update credentials set employer_id = 2 where email = 'marco.verdi@example.com';
-update credentials set employer_id = 3 where email = 'jhonherrera30@icloud.com';
-update credentials set employer_id = 3 where email = 'carlos.gomez@example.com';
+update cDeviceentials set employer_id = 1 where email = 'flaminia.balduini@fastwebnet.it';
+update cDeviceentials set employer_id = 1 where email = 'giulia.rossi@example.com';
+update cDeviceentials set employer_id = 2 where email = 'lucabussi03@gmail.com';
+update cDeviceentials set employer_id = 2 where email = 'marco.verdi@example.com';
+update cDeviceentials set employer_id = 3 where email = 'jhonherrera30@icloud.com';
+update cDeviceentials set employer_id = 3 where email = 'carlos.gomez@example.com';
 
--- 4. Gruppi (devono esistere le credentials, e il progetto)
-insert into app_group(credentials_id, id, project_id, name)values (2, 1, 1, 'Roma');
-insert into app_group(credentials_id, id, project_id, name)values (2, 2, 1, 'Milano');
+-- 4. Gruppi (devono esistere le cDeviceentials, e il progetto)
+insert into app_group(cDeviceentials_id, id, project_id, name)values (2, 1, 1, 'Roma');
+insert into app_group(cDeviceentials_id, id, project_id, name)values (2, 2, 1, 'Milano');
 
 insert into type_of_device(id, name)values(1, 'LoRaWan')
-insert into type_of_device(id, name)values(2, '4Spark 2')
+insert into type_of_device(id, name)values(2, 'FireSensor')
 insert into type_of_device(id, name)values(3, 'MultiSensorRev2 ')
 
 
@@ -76,19 +76,19 @@ insert into device(latitude, longitude, group_id, id, project_id, email_owner, m
 
 
 /* KUCA - sparsi nel nord Italia */
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.440001, 10.995001, 7, 51, 'luca.bussi@outlook.it','11:06:1C:EC:E8:26', 'Red1', 2, 6, true);  -- Verona
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(44.494889, 11.342616, 8, 51, 'luca.bussi@outlook.it','14:06:1C:EC:E8:27', 'Red2', 2, 6, true);  -- Bologna
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.070339, 7.686864, 9, 51, 'luca.bussi@outlook.it','15:06:1C:EC:E8:28', 'Red3', 2, 6, true);   -- Torino
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(46.062008, 11.121083, 10, 51, 'luca.bussi@outlook.it', '23:06:1C:EC:E8:29', 'Red4', 2, 6, true); -- Trento
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.649526, 13.776818, 11, 51, 'luca.bussi@outlook.it', '39:06:1C:EC:E8:30', 'Red5', 2, 6, true); -- Trieste
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(44.801485, 10.327903, 12, 51, 'luca.bussi@outlook.it', '4A:06:1C:EC:E8:31', 'Red6', 2, 6, false); -- Parma
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.440001, 10.995001, 7, 51, 'luca.bussi@outlook.it','11:06:1C:EC:E8:26', 'Device1', 2, 6, true);  -- Verona
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(44.494889, 11.342616, 8, 51, 'luca.bussi@outlook.it','14:06:1C:EC:E8:27', 'Device2', 2, 6, true);  -- Bologna
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.070339, 7.686864, 9, 51, 'luca.bussi@outlook.it','15:06:1C:EC:E8:28', 'Device3', 2, 6, true);   -- Torino
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(46.062008, 11.121083, 10, 51, 'luca.bussi@outlook.it', '23:06:1C:EC:E8:29', 'Device4', 2, 6, true); -- Trento
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.649526, 13.776818, 11, 51, 'luca.bussi@outlook.it', '39:06:1C:EC:E8:30', 'Device5', 2, 6, true); -- Trieste
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(44.801485, 10.327903, 12, 51, 'luca.bussi@outlook.it', '4A:06:1C:EC:E8:31', 'Device6', 2, 6, false); -- Parma
 
 /* Dispositivi aggiuntivi KUCA */
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.184724, 9.158207, 29, 51, 'luca.bussi@outlook.it', '43:07:1C:EC:E8:42', 'Red7', 2, 6, true); -- Pavia
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(44.405650, 8.946256, 30, 51, 'luca.bussi@outlook.it', '54:07:1C:EC:E8:43', 'Red8', 2, 6, true); -- Genova
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.465422, 9.185924, 31, 51, 'luca.bussi@outlook.it', '65:07:1C:EC:E8:44', 'Red9', 2, 6, true); -- Milano
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.327064, 8.419981, 32, 51, 'luca.bussi@outlook.it', '76:07:1C:EC:E8:45', 'Red10', 2, 6, false); -- Novara
-insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(46.498295, 11.354758, 33, 51, 'luca.bussi@outlook.it', '87:07:1C:EC:E8:46', 'Red11', 2, 6, true); -- Bolzano
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.184724, 9.158207, 29, 51, 'luca.bussi@outlook.it', 'AC1F09FFFE1787F1', 'Device1', 2, 6, true); -- Pavia
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, activated)values(44.405650, 8.946256, 30, 51, 'luca.bussi@outlook.it', 'AC1F09FFFE1787F2', 'Device2', 2, false); -- Genova
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(45.465422, 9.185924, 31, 51, 'luca.bussi@outlook.it', 'AC1F09FFFE1787F3', 'Device3', 2, 6, true); -- Milano
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id,  activated)values(45.327064, 8.419981, 32, 51, 'luca.bussi@outlook.it', 'AC1F09FFFE1787F4', 'Device4', 2, false); -- Novara
+insert into device(latitude, longitude, id, project_id, email_owner, mac_address, name, tod_id, operator_id, activated)values(46.498295, 11.354758, 33, 51, 'luca.bussi@outlook.it', 'AC1F09FFFE1787F5', 'Device5', 2, 6, true); -- Bolzano
 
 
 /* JEM - sud e isole */
@@ -140,13 +140,14 @@ insert into type_of_device_specs(specs_id, type_of_device_id)values(14, 2)
 
 -- 6. Reset sequenze
 SELECT setval('app_user_seq', (SELECT MAX(id) FROM app_user));
-SELECT setval('credentials_seq', (SELECT MAX(id) FROM credentials));
+SELECT setval('cDeviceentials_seq', (SELECT MAX(id) FROM cDeviceentials));
 SELECT setval('admin_seq', (SELECT MAX(id) FROM admin));
 SELECT setval('superadmin_seq', (SELECT MAX(id) FROM superadmin));
 SELECT setval('app_group_seq', (SELECT MAX(id) FROM app_group));
 SELECT setval('device_seq', (SELECT MAX(id) FROM device));
 SELECT setval('type_of_device_seq', (SELECT MAX(id) FROM type_of_device));
 SELECT setval('spec_seq', (SELECT MAX(id) FROM spec));
+
 
 
 
